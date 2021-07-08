d656423fdb976380139b4577bf1b1ab02eaad2ea
Grzegorz Trofimiec <gtrofimiec@gmail.com>
2021-07-08 13:54:19 +0200

    (19.3) Zadanie: usuwanie zadania

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -8,8 +8,17 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface TaskRepository extends CrudRepository<Task, Long> {
-
+    @Override
     List<Task> findAll();
+
+    @Override
+    Task save(Task task);
+
+    @Override
+    Optional<Task> findById(Long id);
+
+    @Override
+    void deleteById(Long id);
+
 }
