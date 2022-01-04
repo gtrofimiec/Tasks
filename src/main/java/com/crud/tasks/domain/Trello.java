161974fcadd0c408835eb78997951fb24130beb2
Grzegorz Trofimiec <gtrofimiec@gmail.com>
2022-01-04 17:42:48 +0100

    (32.2) Zadanie: Ustawienie pozostałych wartości

--- src/main/java/com/crud/tasks/domain/Trello.java
+++ src/main/java/com/crud/tasks/domain/Trello.java
@@ -1,18 +1,18 @@
-//package com.crud.tasks.domain;
-//
-//import com.fasterxml.jackson.annotation.JsonProperty;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class Trello {
-//
-//    @JsonProperty("board")
-//    int board;
-//
-//    @JsonProperty("card")
-//    int card;
-//}
+package com.crud.tasks.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Trello {
+
+    @JsonProperty("board")
+    int board;
+
+    @JsonProperty("card")
+    int card;
+}
